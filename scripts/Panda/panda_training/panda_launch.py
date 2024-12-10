07673eb6b70f16e63d4972f64605e0d133a73b1b
XuehuaOvO <xiaoxuehua.xh@gmail.com>
2024-12-10 15:56:30 +0100

    Franak Panda Training

---
+++ scripts/Panda/panda_training/panda_launch.py
@@ -0,0 +1,154 @@
+import os
+from itertools import product
+
+from experiment_launcher import Launcher
+from experiment_launcher.utils import is_local
+
+os.environ["HDF5_USE_FILE_LOCKING"] = "FALSE"
+
+########################################################################################################################
+
+####### Setting ######
+
+# training data folder
+DATASET_SUBDIR = 'Panda-Data' # the folder of the training data files (location: /root/cartpoleDiff/cart_pole_diffusion_based_on_MPD/training_data/CartPole-LMPC)
+
+# training data amount
+TRAINING_DATA_AMOUNT = 113400
+
+# learning parameters
+BATCH_SIZE = 512 # 512
+LEARNING_RATE = 3e-3
+
+EPOCHES = 500 # times that the whole data should be trained
+
+MODEL_SAVED_PATH = '/root/cartpoleDiff/cart_pole_diffusion_based_on_MPD/trained_models/panda_test4_113400'
+EXP_FILE = 'panda_train' # lead to the panda_train.py
+
+# LAUNCHER
+
+LOCAL = is_local()
+TEST = False
+USE_CUDA = True
+
+N_SEEDS = 1
+
+# N_EXPS_IN_PARALLEL = 4
+N_EXPS_IN_PARALLEL = 1
+
+N_CORES = N_EXPS_IN_PARALLEL * 4
+# MEMORY_SINGLE_JOB = 12000
+MEMORY_SINGLE_JOB = 50000
+MEMORY_PER_CORE = N_EXPS_IN_PARALLEL * MEMORY_SINGLE_JOB // N_CORES
+PARTITION = 'gpu' if USE_CUDA else 'amd3,amd2,amd'
+GRES = 'gpu:1' if USE_CUDA else None
+CONDA_ENV = 'mpd'
+
+EXP_NAME = 'train_diffusion_Panda'
+
+launcher = Launcher(
+    exp_name=EXP_NAME,
+    exp_file= EXP_FILE,
+    # project_name='project01234',
+    n_seeds=N_SEEDS,
+    n_exps_in_parallel=N_EXPS_IN_PARALLEL,
+    n_cores=N_CORES,
+    memory_per_core=MEMORY_PER_CORE,
+    days=2,
+    hours=23,
+    minutes=59,
+    seconds=0,
+    partition=PARTITION,
+    conda_env=CONDA_ENV,
+    gres=GRES,
+    use_timestamp=True
+)
+
+########################################################################################################################
+# EXPERIMENT PARAMETERS SETUP
+
+# dataset_subdir_l = [
+#     'EnvSimple2D-RobotPointMass',
+#     'EnvNarrowPassageDense2D-RobotPointMass',
+#     'EnvDense2D-RobotPointMass',
+#     'EnvSpheres3D-RobotPanda',
+
+# ]
+
+dataset_subdir_l = [
+    DATASET_SUBDIR
+]
+
+include_velocity_l = [
+    False
+]
+
+use_ema_l = [
+    True
+]
+
+variance_schedule_l = [
+    'exponential'
+]
+
+n_diffusion_steps_l = [
+    25,
+]
+
+predict_epsilon_l = [
+    True
+]
+
+dim = 32
+
+unet_dim_mults_option_l = [
+    0
+]
+
+
+batch_size = BATCH_SIZE
+lr = LEARNING_RATE
+
+
+wandb_options = dict(
+    wandb_mode='disabled',  # "online", "offline" or "disabled"
+    wandb_entity='scoreplan',
+    wandb_project=EXP_NAME
+)
+
+########################################################################################################################
+# RUN
+
+for dataset_subdir, include_velocity, use_ema, variance_schedule, n_diffusion_steps, predict_epsilon, unet_dim_mults_option in \
+        product(dataset_subdir_l, include_velocity_l, use_ema_l, variance_schedule_l, n_diffusion_steps_l, predict_epsilon_l, unet_dim_mults_option_l):
+
+    launcher.add_experiment(
+        dataset_subdir__=dataset_subdir,
+        include_velocity__=include_velocity,
+        use_ema__=use_ema,
+        variance_schedule__=variance_schedule,
+        n_diffusion_steps__=n_diffusion_steps,
+        predict_epsilon__=predict_epsilon,
+        unet_dim_mults_option__=unet_dim_mults_option,
+
+        lr=lr,
+
+        batch_size=batch_size,
+        # num_train_steps=500000,
+        num_train_steps = TRAINING_DATA_AMOUNT*EPOCHES/BATCH_SIZE, # 246093.75
+
+        model_saving_address = MODEL_SAVED_PATH,
+
+        # steps_til_ckpt=50000,
+        steps_til_ckpt=10000, # 10000
+
+        # steps_til_summary=20000,
+        steps_til_summary=2000,
+
+        **wandb_options,
+        wandb_group=f'{dataset_subdir}-{include_velocity}-{use_ema}-{variance_schedule}-{n_diffusion_steps}-{predict_epsilon}-{unet_dim_mults_option}',
+
+        debug=False,
+    )
+
+launcher.run(LOCAL, TEST)
---
+++ scripts/Panda/panda_training/panda_train.py
@@ -0,0 +1,184 @@
+import torch
+
+from experiment_launcher import single_experiment_yaml, run_experiment
+from mpd import trainer
+from mpd.models import UNET_DIM_MULTS, ConditionedTemporalUnet
+from mpd.trainer import get_dataset, get_model, get_loss, get_summary
+from mpd.trainer.trainer import get_num_epochs
+from torch_robotics.torch_utils.seed import fix_random_seed
+from torch_robotics.torch_utils.torch_utils import get_torch_device
+
+# from classifier_free_guidance_pytorch import TextConditioner
+# from classifier_free_guidance_pytorch import classifier_free_guidance_class_decorator
+
+@single_experiment_yaml
+def experiment(
+    ########################################################################
+    # Dataset
+    dataset_subdir: str = 'Panda-Data',
+    include_velocity: bool = False,
+
+    ########################################################################
+    # Diffusion Model
+    diffusion_model_class: str = 'GaussianDiffusionModel',
+    variance_schedule: str = 'exponential',  # cosine
+    n_diffusion_steps: int = 25,
+    predict_epsilon: bool = True,
+
+    # Unet
+    unet_input_dim: int = 32,
+    unet_dim_mults_option: int = 1,
+
+    ########################################################################
+    # Loss
+    loss_class: str = 'GaussianDiffusionCartPoleLoss',
+
+    # Training parameters
+    batch_size: int = 32,
+    lr: float = 1e-4,
+    num_train_steps: int = 5000, # 50000
+
+    use_ema: bool = True,
+    use_amp: bool = False,
+
+    # model saving address
+    model_saving_address = '/root/cartpoleDiff/cart_pole_diffusion_based_on_MPD/data_trained_models',
+
+    # Summary parameters
+    steps_til_summary: int = 2000,
+    summary_class: str = 'SummaryTrajectoryGeneration',
+
+    steps_til_ckpt: int = 5000,
+
+    ########################################################################
+    device: str = 'cuda',
+
+    debug: bool = True,
+
+    ########################################################################
+    # MANDATORY
+    seed: int = 0,
+    results_dir: str = 'logs',
+
+    ########################################################################
+    # WandB
+    wandb_mode: str = 'disabled',  # "online", "offline" or "disabled"
+    wandb_entity: str = 'scoreplan',
+    wandb_project: str = 'test_train',
+    **kwargs
+):
+    fix_random_seed(seed)
+
+    device = get_torch_device(device=device)
+    print(f'device --{device}')
+    tensor_args = {'device': device, 'dtype': torch.float32}
+
+    # Dataset
+    train_subset, train_dataloader, val_subset, val_dataloader = get_dataset(
+        dataset_class='InputsDataset',
+        include_velocity=include_velocity,
+        dataset_subdir=dataset_subdir,
+        batch_size=batch_size,
+        results_dir=results_dir,
+        save_indices=True,
+        tensor_args=tensor_args
+    )
+
+    print(f'train_subset -- {len(train_subset.indices)}')
+    dataset = train_subset.dataset
+    print(f'dataset -- {dataset}')
+
+    # # texts
+    # repo = git.Repo('.', search_parent_directories=True)
+    # dataset_base_dir = os.path.join(repo.working_dir, 'data_trajectories')
+    # base_dir = os.path.join(dataset_base_dir, dataset_subdir)
+    # condition_texts = torch.load(os.path.join(base_dir, 'x-collecting_list_6400.pt'),map_location=tensor_args['device'])
+
+
+    # Model
+    diffusion_configs = dict(
+        variance_schedule=variance_schedule,
+        n_diffusion_steps=n_diffusion_steps,
+        predict_epsilon=predict_epsilon,
+    )
+
+    unet_configs = dict(
+        state_dim=dataset.state_dim,
+        n_support_points=dataset.n_support_points,
+        unet_input_dim=unet_input_dim,
+        dim_mults=UNET_DIM_MULTS[unet_dim_mults_option],
+    )
+
+    model = get_model(
+        model_class=diffusion_model_class,
+        model=ConditionedTemporalUnet(**unet_configs), # TemporalUnet(**unet_configs)
+        tensor_args=tensor_args,
+        **diffusion_configs,
+        **unet_configs
+        # text_condition_type = 'film', 
+        # text_condition_model_types = ('t5'),
+        # text_condition_hidden_dims = (32, 64, 128),
+        # text_condition_cond_drop_prob = 0.25,
+    )
+
+    # Loss
+    loss_fn = val_loss_fn = get_loss(
+        loss_class=loss_class
+    )
+
+    # text conditioner
+    # text_conditioner = TextConditioner(
+    # model_types = 't5',    
+    # hidden_dims = (32,64,128),
+    # hiddens_channel_first = False,
+    # cond_drop_prob = 0.25  # conditional dropout 20% of the time, must be greater than 0. to unlock classifier free guidance
+    # ).cuda()
+
+    # Summary
+    # summary_fn = get_summary(
+    #     summary_class=summary_class,
+    # )
+
+    # Train
+    trainer.train(
+        model=model,
+        train_dataloader=train_dataloader,
+        train_subset=train_subset,
+        val_dataloader=val_dataloader,
+        val_subset=train_subset,
+        epochs=get_num_epochs(num_train_steps, batch_size, len(dataset)),
+        model_dir=results_dir,
+        # summary_fn=summary_fn,
+        lr=lr,
+        loss_fn=loss_fn,
+        val_loss_fn=val_loss_fn,
+        steps_til_summary=steps_til_summary,
+        steps_til_checkpoint=steps_til_ckpt,
+        clip_grad=True,
+        use_ema=use_ema,
+        use_amp=use_amp,
+        debug=debug,
+        model_saving_address = model_saving_address,
+        #text_conditioner = text_conditioner,
+        tensor_args=tensor_args
+    )
+
+    # print(f'DEBUG MODE: {debug}')
+
+    # print(f'kwargs: {kwargs}')
+
+    # filename = os.path.join(results_dir, 'log_' + str(seed) + '.txt')
+    # out_str = f'Running experiment with seed {seed}, env {env}, ' \
+    #           f'env_param {env_param}, a {a}, ' \
+    #           f'boolean_param {boolean_param}, some_default_param {some_default_param}'
+    # print(out_str)
+    # with open(filename, 'w') as file:
+    #     file.write('Some logs in a log file.\n')
+    #     file.write(out_str)
+
+    # wandb.log({'seed': seed}, step=1)
+
+
+if __name__ == '__main__':
+    # Leave unchanged
+    run_experiment(experiment)
