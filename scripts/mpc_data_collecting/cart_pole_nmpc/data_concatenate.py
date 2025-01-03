5067c39f83cd3acc7b96cd2ef11248b0fe7dd6d4
XuehuaOvO <xiaoxuehua.xh@gmail.com>
2025-01-03 23:14:24 +0100

    new folder name

--- scripts/mpc_data_collecting/cart_pole_nmpc/data_concatenate.py
+++ scripts/mpc_data_collecting/cart_pole_nmpc/data_concatenate.py
@@ -28,33 +28,36 @@ tensor_j_list = []
 
 # u data cat
 for idx in data_idxs:
-    file_name = 'u_data_' + 'idx-' + str(idx) + '_test1.pt'
+    file_name = 'pure_u_data_' + 'idx-' + str(idx) + '_test1.pt'
     file_path = os.path.join(FOLDER_PATH , file_name)
     tensor = torch.load(file_path)
     tensor_u_list.append(tensor)
 
 
 concatenated_u_tensor = torch.cat(tensor_u_list, dim=0)
-torch.save(concatenated_u_tensor, os.path.join(FOLDER_PATH , f'u_data_' + 'cat' + '_test1.pt'))
+print(f'u size -- {concatenated_u_tensor.size()}')
+torch.save(concatenated_u_tensor, os.path.join(FOLDER_PATH , f'pure_u_data_' + 'cat' + '_test1.pt'))
 
 # x data cat
 for idx in data_idxs:
-    file_name = 'x_data_' + 'idx-' + str(idx) + '_test1.pt'
+    file_name = 'pure_x_data_' + 'idx-' + str(idx) + '_test1.pt'
     file_path = os.path.join(FOLDER_PATH , file_name)
     tensor = torch.load(file_path)
     tensor_x_list.append(tensor)
 
 
 concatenated_x_tensor = torch.cat(tensor_x_list, dim=0)
-torch.save(concatenated_x_tensor, os.path.join(FOLDER_PATH , f'x_data_' + 'cat' + '_test1.pt'))
+print(f'x size -- {concatenated_x_tensor.size()}')
+torch.save(concatenated_x_tensor, os.path.join(FOLDER_PATH , f'pure_x_data_' + 'cat' + '_test1.pt'))
 
 # j data cat
 for idx in data_idxs:
-    file_name = 'j_data_' + 'idx-' + str(idx) + '_test1.pt'
+    file_name = 'pure_j_data_' + 'idx-' + str(idx) + '_test1.pt'
     file_path = os.path.join(FOLDER_PATH , file_name)
     tensor = torch.load(file_path)
     tensor_j_list.append(tensor)
 
 
 concatenated_j_tensor = torch.cat(tensor_j_list, dim=0)
-torch.save(concatenated_j_tensor, os.path.join(FOLDER_PATH , f'j_data_' + 'cat' + '_test1.pt'))
+print(f'j size -- {concatenated_j_tensor.size()}')
+torch.save(concatenated_j_tensor, os.path.join(FOLDER_PATH , f'pure_j_data_' + 'cat' + '_test1.pt'))
