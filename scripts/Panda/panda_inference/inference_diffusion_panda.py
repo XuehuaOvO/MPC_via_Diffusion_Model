46675fbafbb3bf00e67e128adb8e79c8771960cd
XuehuaOvO <xiaoxuehua.xh@gmail.com>
2025-04-24 16:59:36 +0200

    update

--- scripts/Panda/panda_inference/inference_diffusion_panda.py
+++ scripts/Panda/panda_inference/inference_diffusion_panda.py
@@ -17,7 +17,6 @@ import torch
 import random
 import casadi as ca
 import time
-import imageio
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_ROOT = os.path.abspath(os.path.join(CURRENT_DIR, '../../../')) 
